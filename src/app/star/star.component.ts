d5f6d7c9d1567889ffdeeb768cc8a1ddab450f34
xushuo <893804934@qq.com>
2017-05-28 14:08:08 +0800

    添加input属性监听的 ngonchanges方法，简化代码，并且添加星级组件的动态的编辑状态

--- src/app/star/star.component.ts
+++ src/app/star/star.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output, OnChanges, SimpleChanges} from '@angular/core';
 import construct = Reflect.construct;
 
 @Component({
@@ -6,19 +6,34 @@ import construct = Reflect.construct;
   templateUrl: './star.component.html',
   styleUrls: ['./star.component.css']
 })
-export class StarComponent implements OnInit {
+export class StarComponent implements OnInit, OnChanges {
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.stars=[];
+    for(var i=1; i<=5; i++){
+      this.stars.push(i>this.rating);
+    }
+  }
 
   @Input()
   private rating:number=0;
   private stars:boolean[];
+  @Input()
+  private readonly: boolean =true;
+
+  @Output()
+  ratingChange: EventEmitter<number> =new EventEmitter();
+
   constructor() { }
 
   ngOnInit() {
-    this.stars=[];
-    for(var i=1; i<=5; i++){
-        this.stars.push(i>this.rating);
-    }
 
   }
 
+  editStar(index: number){
+    if(!this.readonly){
+      this.rating = index+1;
+      this.ratingChange.emit(this.rating);
+    }
+  }
 }
